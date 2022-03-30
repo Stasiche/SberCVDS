2cb405ccacada1d1764c8e1d54d7d7e9695e668f
Stanislav_Lebedev <lebedevsf239@gmail.com>
2022-03-30 23:21:31 +0300

    newest results

--- analyze_models.py
+++ analyze_models.py
@@ -10,30 +10,39 @@ from torch.utils.data import DataLoader
 import numpy as np
 
 from src.breed_convector import BreedConvector
-from src.utils import load_model, stopwatch, eval_model, calc_parameters, plot_errors
+from src.utils import load_model, stopwatch, eval_model, calc_parameters, plot_errors, plot_confusion_matrix
 import csv
 
 api = wandb.Api()
 convector = BreedConvector()
 val_dataset = ImageWoofDataset('val')
-valdata = DataLoader(val_dataset, batch_size=50, shuffle=False)
+valdata = DataLoader(val_dataset, batch_size=100, shuffle=False)
 os.makedirs('errors', exist_ok=True)
 
-runs_list = ['3h4ou7sh', '3bfpypi0', '116xetcy']
+runs_list = ['1fx3trl2', '3ap725ro', '354r2rsn', '3g7bc5nw', '2vbd1dph', 'kb1m9k2e']
+
+run_name_to_model = {
+    '1fx3trl2': 7,
+    '3ap725ro': 'model',
+    '354r2rsn': 'model',
+    '3g7bc5nw': 5,
+    '2vbd1dph': 5,
+    'kb1m9k2e': 'model',
+}
 
 with open('analyze.csv', 'w') as f:
     writer = csv.writer(f)
     writer.writerow(['model_name', 'parameters_number(M)', 'model_size(MB)', 'accuracy', 'time(s)', 'rps'])
     for run_name in runs_list:
         run = api.run(f'stasiche/SberCVDS/{run_name}')
-        model = load_model(run)
+        model = load_model(run, run_name_to_model)
         with stopwatch() as t:
             acc, answers = eval_model(model, valdata)
             eval_time = t()
         params_num = calc_parameters(model)
-        model_size = round(getsize(join('models', run_name, 'model.pt'))/1e6, 1)
-        writer.writerow([run.config["model_name"], round(params_num/1e6, 1), model_size, round(acc, 4),
-                         round(eval_time, 0),  round(len(valdata) / eval_time, 3)])
+        model_size = round(getsize(join('models', run_name, f'{run_name_to_model[run_name]}.pt')) / 1e6, 1)
+        writer.writerow([run.config["model_name"], round(params_num / 1e6, 1), model_size, round(acc, 4),
+                         round(eval_time, 0), round(len(valdata) / eval_time, 3)])
 
         makedirs(join('errors', run.config['model_name']), exist_ok=True)
         base_path = valdata.dataset.datapath
@@ -41,4 +50,7 @@ with open('analyze.csv', 'w') as f:
             pred, gt = answers[:, indx]
             img_path = valdata.dataset.data[indx][0]
 
-            plot_errors(pred, gt, join(base_path, img_path), join('errors', run.config["model_name"], str(i)), convector)
+            plot_errors(pred, gt, join(base_path, img_path), join('errors', run.config["model_name"], str(i)),
+                        convector)
+        plot_confusion_matrix(answers[0], answers[1], join('errors', run.config["model_name"], 'confusion_matrix'),
+                              convector)
--- src/utils.py
+++ src/utils.py
@@ -11,6 +11,8 @@ import numpy as np
 
 import matplotlib.pyplot as plt
 from PIL import Image
+from sklearn.metrics import confusion_matrix
+import seaborn as sns
 
 
 @torch.no_grad()
@@ -46,10 +48,29 @@ def plot_errors(predicted_label, gt_label, img_path, save_path, convector):
     fig.savefig(f'{save_path}.jpeg', bbox_inches='tight')
 
 
-def load_model(run):
+def plot_confusion_matrix(predicted_label, gt_label, save_path, convector):
+    cf_matrix = confusion_matrix(gt_label, predicted_label)
+
+    fig, ax = plt.subplots()
+    ax = sns.heatmap(cf_matrix, annot=True, cmap='Blues', fmt='g')
+    ax.set_xlabel('\nPredicted Breed')
+    ax.set_ylabel('Actual Breed ')
+    ax.tick_params(axis='x', rotation=80)
+    ax.tick_params(axis='y', rotation=15)
+    ax.tick_params(axis='both', which='major', labelsize=10)
+
+    labels = [convector.index_to_breed[i] for i in range(10)]
+    ax.xaxis.set_ticklabels(labels)
+    ax.yaxis.set_ticklabels(labels)
+
+    fig.savefig(f'{save_path}.jpeg', bbox_inches='tight', pad_inches=1)
+    plt.close()
+
+
+def load_model(run, run_name_to_model):
     model = getattr(models, run.config['model_name'])()
     model.classifier[-1] = Linear(model.classifier[-1].in_features, 10)
-    restore_model(model, run.id)
+    restore_model(model, run.id, run_name_to_model)
     model.eval()
     return model
 
@@ -60,9 +81,10 @@ def stopwatch() -> float:
     yield lambda: perf_counter() - start
 
 
-def restore_model(model, run_name):
-    local_model_path = join('models', run_name, 'model.pt')
+def restore_model(model, run_name, run_name_to_model):
+    model_name_file = f'{run_name_to_model[run_name]}.pt'
+    local_model_path = join('models', run_name, model_name_file)
 
     if not exists(local_model_path):
-        wandb.restore('model.pt', f'stasiche/SberCVDS/{run_name}', root=dirname(local_model_path))
+        wandb.restore(model_name_file, f'stasiche/SberCVDS/{run_name}', root=dirname(local_model_path))
     model.load_state_dict(torch.load(local_model_path, map_location=torch.device('cpu')))
